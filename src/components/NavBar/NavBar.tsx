cc21a268f6b46810b4ad1d8a24f44501b5bc9a15
unknown <danrlei_vieira@hotmail.com>
2025-10-21 20:44:12 -0300

    refactor: add responsiveness to NavBar for different screen sizes

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,26 +6,24 @@ import { Link } from "react-router-dom";
 
 const NavBar: React.FC = () => {
   return (
-    <>
-      <nav className="nav-bar">
-        <div>
-          <Link to="/">
-            <img src={marcaTS} alt="Marca ShopTS" />
-          </Link>
-        </div>
-        <div className="search-form">
-          <input type="text" placeholder="Search" name="search" />
-          <button type="button" className="search-button">
-            <FaMagnifyingGlass className="search-icon" />
-          </button>
-        </div>
-        <div>
-          <button type="button" className="cart-button">
-            <BsCart className="cart-icon" />
-          </button>
-        </div>
-      </nav>
-    </>
+    <nav className="nav-bar">
+      <div>
+        <Link to="/">
+          <img src={marcaTS} alt="Marca ShopTS" />
+        </Link>
+      </div>
+      <div className="search-form">
+        <input type="text" placeholder="Search" name="search" />
+        <button type="button" className="search-button">
+          <FaMagnifyingGlass className="search-icon" />
+        </button>
+      </div>
+      <div>
+        <button type="button" className="cart-button">
+          <BsCart className="cart-icon" />
+        </button>
+      </div>
+    </nav>
   );
 };
 
