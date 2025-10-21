9ad26564a360806e60d4b3c75e248ff55d48241d
unknown <danrlei_vieira@hotmail.com>
2025-10-20 21:05:51 -0300

    feat: implement product grid rendering, mock data, and final layout adjustments

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,8 +1,35 @@
-const ProductCard: React.FC = () => {
+import "./ProductCard.scss";
+import type { Product } from "../../types/Product";
+import { BsCart } from "react-icons/bs";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   return (
-    <>
-      <h1>Product card.</h1>
-    </>
+    <div className="product-card">
+      <img
+        src={product.imageURL}
+        alt={`Description: ${product.description}`}
+        className="product-img"
+      />
+      <div className="product-name">
+        <h3 className="name">{product.name}</h3>
+      </div>
+      <p className="product-price">
+        {product.price.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </p>
+      <div className="product-btn">
+        <button className="btn-buy">Buy</button>
+        <button className="btn-cart">
+          Add <BsCart />
+        </button>
+      </div>
+    </div>
   );
 };
 
--- src/components/ProductGrid/ProductGrid.tsx
+++ src/components/ProductGrid/ProductGrid.tsx
@@ -1,14 +1,20 @@
-import "./ProductGrid.scss"
+import "./ProductGrid.scss";
+import type { Product } from "../../types/Product";
+
+import productsData from "../../data/products.json";
+import ProductCard from "../ProductCard/ProductCard";
 
 const ProductGrid: React.FC = () => {
+  const products: Product[] = productsData as Product[];
+
   return (
     <section className="section-container">
-      <div>
-        <h2>Offers of the day</h2>
-      </div>
+      <h2>Offers of the day</h2>
 
       <div className="container">
-
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </div>
     </section>
   );
