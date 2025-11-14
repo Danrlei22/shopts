1a213759c073b41e62066a51e2f9bdf59903e889
unknown <danrlei_vieira@hotmail.com>
2025-11-14 15:23:02 -0300

    feat: implement CategoryList component and load data from JSON file

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,10 +1,25 @@
-import "./Categories.scss"
+import "./Categories.scss";
+import categoriesData from "../../data/categories.json";
+
+interface Category {
+  id: number;
+  name: string;
+}
 
 const Categories: React.FC = () => {
+  const categories: Category[] = categoriesData as Category[];
   return (
     <section>
       <div className="container-h2">
         <h2 className="section-h2">Categories</h2>
+      </div>
+
+      <div className="categories-container">
+        {categories.map((categories) => (
+          <button key={categories.id} className="btn-category">
+            {categories.name}
+          </button>
+        ))}
       </div>
     </section>
   );
