c06fc815a62d0e27b254a7b42a29aad383de0b6c
unknown <danrlei_vieira@hotmail.com>
2026-04-01 13:44:57 -0300

    test(navbar): add input typing test for NavBar search

--- src/components/NavBar/NavBar.test.tsx
+++ src/components/NavBar/NavBar.test.tsx
@@ -5,8 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import { store } from "../../store";
 import { test, expect } from "vitest";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 
-test("render input search", () => {
+test("render input search", async () => {
   render(
     <Provider store={store}>
       <BrowserRouter>
@@ -17,5 +18,8 @@ test("render input search", () => {
 
   const input = screen.getByPlaceholderText("Search");
 
-  expect(input).toBeInTheDocument();
+  await userEvent.type(input, "nike");
+
+  //expect(input).toBeInTheDocument();
+  expect(input).toHaveValue("nike");
 });
