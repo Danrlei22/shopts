cdfbd2ffcc399c7f59a9e639a43dad81e189acb2
unknown <danrlei_vieira@hotmail.com>
2026-06-03 16:48:15 -0300

    test(cypress): alter quantity of product in cart

--- cypress/e2e/cart.cy.tsx
+++ cypress/e2e/cart.cy.tsx
@@ -22,4 +22,21 @@ describe("Cart flow", () => {
     cy.get(".cart-items").should("be.visible");
     cy.get(".cart-items").should("have.length", 1);
   });
+
+  it("Alter quantity of product in cart", () => {
+    cy.visit("http://localhost:5173/");
+
+    cy.get("input[placeholder='Search']").type("tv");
+    cy.get("button[aria-label='search']").click();
+    cy.get(".product-card").first().click();
+    cy.get(".btn-cart").click();
+    cy.get(".cart-button").click();
+    cy.get(".cart-items").should("be.visible");
+    cy.get(".cart-items").should("have.length", 1);
+    cy.get(".item-quantity").should("have.value", "1");
+    for (let i = 2; i <= 5; i++) {
+      cy.get(".item-more-quantity").click();
+    }
+    cy.get(".item-quantity").should("have.value", "5");
+  });
 });
